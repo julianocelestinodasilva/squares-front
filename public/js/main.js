ce2d518dc4111e0cd7678ecc25eff6d86eed267e
Juliano <jcelestino@raiadrogasil.com.br>
2017-07-28 16:06:38 -0300

    Exclusão códigos desnecessários

--- public/js/main.js
+++ public/js/main.js
@@ -10,18 +10,6 @@ angular.module('squares-front', ['minhasDiretivas', 'ngAnimate', 'ngRoute', 'ngR
             controller: 'FotosController'
         });
 
-        // foto no singular!
-
-        $routeProvider.when('/fotos/new', {
-            templateUrl: 'partials/foto.html',
-            controller: 'FotoController'
-        });
-
-        $routeProvider.when('/fotos/edit/:fotoId', {
-            templateUrl: 'partials/foto.html',
-            controller: 'FotoController'
-        });
-
         $routeProvider.otherwise({ redirectTo: '/fotos' });
 
     });
