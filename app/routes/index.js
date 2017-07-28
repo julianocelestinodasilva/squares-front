b9f1707f90f3a016ce619f849718bcf3b8b2daa3
Juliano <jcelestino@raiadrogasil.com.br>
2017-07-28 16:42:16 -0300

    Exclusão códigos desnecessários

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,17 +4,8 @@ var api = require('../api'),
 module.exports  = function(app) {
     
     app.route('/v1/fotos')
-        .post(api.adiciona)
         .get(api.lista);
 
-    app.route('/v1/fotos/:fotoId')
-        .delete(api.remove)
-        .get(api.busca)
-        .put(api.atualiza);
-
-    app.get('/v1/grupos', api.listaGrupos)
-    app.get('/v1/fotos/grupo/:grupoId', api.listaPorGrupo);
-        
 
     // habilitando HTML5MODE
     app.all('/*', function(req, res) {
