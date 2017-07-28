3d1ce3e4c3a90d7d8b1b8a56e0a50791b88055a7
Juliano <jcelestino@raiadrogasil.com.br>
2017-07-28 16:33:55 -0300

    Exclusão códigos desnecessários

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // public/js/main.js
 
-angular.module('squares-front', ['minhasDiretivas', 'ngAnimate', 'ngRoute', 'ngResource', 'meusServicos'])
+angular.module('squares-front', ['ngAnimate', 'ngRoute', 'ngResource', 'meusServicos'])
     .config(function($routeProvider, $locationProvider) {
 
         $locationProvider.html5Mode(true);
